f791f7bc8fe570d9fa91960935628cc064bfe07e
swisscomandy <andy.shi@swisscom-labs.com>
2016-10-19 11:24:11 -0700

    first commit

---
+++ iptables_manager.go
@@ -0,0 +1,52 @@
+package iptables_manager
+
+import "net"
+
+//go:generate counterfeiter -o fake_chain/fake_chain.go . Chain
+type Chain interface {
+	Setup(containerID, bridgeName string, ip net.IP, network *net.IPNet, externalIp net.IP, portRange string) error
+	Teardown(containerID string) error
+}
+
+type IPTablesManager struct {
+	chains []Chain
+}
+
+func New() *IPTablesManager {
+	return &IPTablesManager{}
+}
+
+func (mgr *IPTablesManager) AddChain(chain Chain) *IPTablesManager {
+	mgr.chains = append(mgr.chains, chain)
+
+	return mgr
+}
+
+func (mgr *IPTablesManager) ContainerSetup(containerID, bridgeName string, ip net.IP, network *net.IPNet, externalIp net.IP, portRange string) error {
+	if err := mgr.ContainerTeardown(containerID); err != nil {
+		return err
+	}
+
+	for index, chain := range mgr.chains {
+		if err := chain.Setup(containerID, bridgeName, ip, network, externalIp, portRange); err != nil {
+			for i := 0; i < index; i++ {
+				mgr.chains[i].Teardown(containerID)
+			}
+			return err
+		}
+
+	}
+
+	return nil
+}
+
+func (mgr *IPTablesManager) ContainerTeardown(containerID string) error {
+	var lastErr error
+	for _, chain := range mgr.chains {
+		if err := chain.Teardown(containerID); err != nil {
+			lastErr = err
+		}
+	}
+
+	return lastErr
+}
